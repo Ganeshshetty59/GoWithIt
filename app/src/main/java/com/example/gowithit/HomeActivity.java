4957b4e91d02f47844990f5f8c08872f508e71c7
Ganeshshetty59 <ganeshshetty7073@gmail.com>
2022-07-06 11:54:06 +0200

    gowithit

--- app/src/main/java/com/example/gowithit/HomeActivity.java
+++ app/src/main/java/com/example/gowithit/HomeActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -108,6 +109,7 @@ public class HomeActivity extends AppCompatActivity {
                 Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
+                Toast.makeText(HomeActivity.this, "LOGOUT Successful", Toast.LENGTH_SHORT).show();
 
             }
         });
