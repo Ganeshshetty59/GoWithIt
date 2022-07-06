321c990ac0abab08f722b25de7628298d140f8fd
Ganeshshetty59 <ganeshshetty7073@gmail.com>
2022-07-06 11:09:13 +0200

    gowithit

---
+++ app/src/main/java/com/example/gowithit/ListenMusic.java
@@ -0,0 +1,14 @@
+package com.example.gowithit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListenMusic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listen_music);
+    }
+}
