2bf0fcd3da09959c8a2d9993f47dbb1ec9c91e1b
Ganeshshetty59 <ganeshshetty7073@gmail.com>
2022-07-22 07:04:05 +0200

    gowithit

--- app/src/main/java/com/example/gowithit/ListenMusic.java
+++ app/src/main/java/com/example/gowithit/ListenMusic.java
@@ -7,12 +7,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -28,7 +26,7 @@ public class ListenMusic extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_listen_music);
 
         recyclerView = findViewById(R.id.recycler_view);
         noMusicTextView = findViewById(R.id.no_songs_text);
--- app/src/main/java/com/example/gowithit/MusicListAdapter.java
+++ app/src/main/java/com/example/gowithit/MusicListAdapter.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
