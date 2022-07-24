8276317722c0cb178c19ee55876f5c46815f4810
Ganeshshetty59 <ganeshshetty7073@gmail.com>
2022-07-24 11:30:29 +0200

    gowithit

--- app/src/main/java/com/example/gowithit/RetrieveHotel.java
+++ app/src/main/java/com/example/gowithit/RetrieveHotel.java
@@ -1,12 +1,25 @@
 package com.example.gowithit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ListView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class RetrieveHotel extends AppCompatActivity {
     ListView listView;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+    ArrayList<String> list;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +27,21 @@ public class RetrieveHotel extends AppCompatActivity {
         setContentView(R.layout.activity_retrieve_hotel);
 
         listView=findViewById(R.id.listview);
+        database=FirebaseDatabase.getInstance();
+        reference=database.getReference("Hotels");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds:dataSnapshot.getChildren()){
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
